dc9a3d598371206044acc8fd1b8d3d1314df45de
nrhuang <nhuang@infoblox.com>
2026-08-10 13:23:59 -0700

    IBIQ-2: enable checking off todos

--- app.js
+++ app.js
@@ -22,19 +22,36 @@ function render() {
   list.innerHTML = '';
   for (const todo of todos) {
     const li = document.createElement('li');
+    li.className = todo.done ? 'done' : '';
+
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.checked = todo.done;
+    checkbox.id = `todo-${todo.id}`;
+    checkbox.addEventListener('change', () => toggleTodo(todo.id));
 
     const label = document.createElement('label');
+    label.htmlFor = checkbox.id;
     label.textContent = todo.text;
 
-    li.append(label);
+    li.append(checkbox, label);
     list.appendChild(li);
   }
 }
 
 function addTodo(text) {
-  todos.push({ id: Date.now(), text });
+  todos.push({ id: Date.now(), text, done: false });
   saveTodos();
   render();
+}
+
+function toggleTodo(id) {
+  const todo = todos.find(t => t.id === id);
+  if (todo) {
+    todo.done = !todo.done;
+    saveTodos();
+    render();
+  }
 }
 
 form.addEventListener('submit', (e) => {
